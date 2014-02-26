bb872b809b68a42153e801d8d37f0e5b151b9fc5
jhpy1024 <jhpy1024@gmail.com>
2014-02-26 22:02:04 +0000

    Using freeglut for input/window instead of SFML

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,29 @@
-#include "Game.hpp"
+#include <iostream>
 
-int main()
+#include <GL/glut.h>
+
+void handleInput(unsigned char key, int x, int y)
 {
-    Game game(640, 480, "glfPong | 1GAM February 2014");
-    game.run();
+
+}
+
+void render()
+{
+    glClearColor(1.f, 0.f, 0.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 480);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutCreateWindow("Test");
+    glutKeyboardFunc(&handleInput);
+    glutDisplayFunc(&render);
+    glutMainLoop();
+
+    return 0;
 }
